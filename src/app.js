c57084b28a55a60b9543ca3e20fe2bd8d3dc75e7
MEHMET ŞENTÜRK <m.sntrk86@hotmail.com>
2021-10-23 21:24:32 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,36 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+
+{
+   let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//pascalCasing
+let konutKredileri = ["Konut kredisi","Emlak Konut kredisi","Kamu Konut kredisi","Özel Konut kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+{/* <ul>
+    <li>Konut kredisi</li>
+    <li>Emlak Konut kredisi</li>
+    <li>Kamu Konut kredisi</li>
+</ul>*/}
+
+console.log(konutKredileri)
